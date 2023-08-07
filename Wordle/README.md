fcd091c07529ac0f5905d41b4439edd5bd7682d6
ArsalanMoravvej <moravvejmohammad97@gmail.com>
2023-08-07 17:50:38 +0330

    comments and raedme

--- Wordle/README.md
+++ Wordle/README.md
@@ -1,1 +1,22 @@
-Wordle
+Wordle Game
+Introduction
+
+Wordle is a simple word-guessing game. The game prompts the player to guess a word of a specified length. The player has a limited number of attempts to guess the word correctly. The game provides feedback on each guess to help the player narrow down the correct answer.
+
+Prerequisites
+Python 3.8 or above
+Random module (included in Python standard library)
+How to Play
+Open the terminal or command prompt.
+Navigate to the directory where the game script is located.
+Run the script using the Python interpreter:
+python wordle.py <word_length>
+
+Replace <word_length> with an integer value between 5 and 8 (inclusive). This specifies the length of the word to be guessed.
+The game will start and prompt you to enter a word of the specified length.
+Enter your guess and press Enter.
+The game will provide feedback on each guess and prompt you to make another guess.
+Keep guessing until you either guess the word correctly or run out of attempts.
+The game will announce whether you won or lost, and show the number of attempts made.
+Support
+
--- Wordle/Wordle.py
+++ Wordle/Wordle.py
@@ -1,10 +1,12 @@
 import sys
 from random import choice
 
+#clearing the last line in commandline.
 def clear_last_line():
-    sys.stdout.write("\033[F")  # Move the cursor to the previous line
-    sys.stdout.write("\033[K")  # Clear the line
-    
+    sys.stdout.write("\033[F")  # Move the cursor to the previous line.
+    sys.stdout.write("\033[K")  # Clear the line.
+
+#this function is tailored to print colored format strings in the commandline.
 def print_with_background_color(text, background_color, new_line = False):
     colors = {
         'black': '\033[40m',
@@ -19,53 +21,83 @@ def print_with_background_color(text, background_color, new_line = False):
     reset = '\033[0m'
     sys.stdout.write(colors[background_color] + text + reset)
     sys.stdout.flush()
+    
+    #condition for making a new line.
     if new_line:
-        print("\n", end="")
+        print("")
 
-
+# getting user guess.
 def get_guess(mode):
+
+    #ensuring that it's in valid length.
     guess = ""
     while len(guess) != mode:
         guess = input(f"Input a {mode}-letter word: ")
+
+        #clearing the last line after the user has entered their guess.
         clear_last_line()
     return guess
 
+# checking the user guess, calculate the score and status list items.
 def check_guess(guess, word, status, mode):
     score = 0
     for i in range(mode):
+
         for j in range(mode):
+
             if guess[i] == word[i]:
+                
                 status[i] = 2
                 break
+
             elif guess[i] == word[j]:
+
                 status[i] = 1
+
         score += status[i]
+
+    # returning calculated status and score.
     return status, score
 
+# printing the user guess in a colored format.
 def print_word(guess, status, mode):
+
     for i in range(mode):
+
         if status[i] == 2:
+
             print_with_background_color(" " + guess[i] + " ", 'green')
+        
         elif status[i] == 1:
+
             print_with_background_color(" " + guess[i] + " ", 'yellow')
+
         else:
+
             print_with_background_color(" " + guess[i] + " ", 'red')
 
         print(" ", end="")
 
     print("\n\n", end="")
 
+# game function (called when the game is initiated.).
 def game(mode):
 
+    # openinng a (.txt) file according to the specified mode consisting of 1000 words of len(mode).
     file_path = f"files/{mode}.txt"
     with open(file_path, "r") as file:
         contents = file.read()
 
+    # chosing a pseudorandom word within the 1000 words.
     word = choice(contents.split("\n"))
 
+    #allowing one more guess than the length of the word.
     guesses = mode + 1
+
+    # game result (initialy False).
     won = False
-    
+
+    # print greeting, using ANSI color codes to demonstrate.
     print("")
     print("")
     print("             ", end="")
@@ -74,39 +106,68 @@ def game(mode):
     print_with_background_color(f"You have {guesses} tries to guess the {mode}-letter word", 'magenta', new_line=True)
     print("")
 
+    # main game loop, one iteration for each guess.
     for i in range(guesses):
+
+        #obtaining user's guess.
         guess = get_guess(mode)
+
+        #a list to hold guess status, initially set to zero.
         status = [0 for _ in range(mode)]
+
+        # Calculate score and status for the guess.
         status, score = check_guess(guess, word, status, mode)
+
+        # Print the guess.
         print(f"Guess {i+1} = ", end="")
         print_word(guess, status, mode)
         
+        # if they guessed it exactly right, terminate loop.
         if score == mode * 2:
             won = True
             break
     
-    return won, i, word
+    #returning the result, number of attempts and the actual word.
+    return [won, i, word]
 
+#showing the results.
+def show_results(res, i, word):
+    if res:
 
-def main():
-    try:
-
-        initiate = int(sys.argv[1])
-    except:
-
-        print_with_background_color("Invalid entry!", 'red', new_line=True)
-        print_with_background_color("Usage: ./wordle.py [wordsize]", 'blue', new_line=True)
+        # Announcement of winnig and number of attempts.
+        print(f"You won!\nNumber of attempt: {i + 1}")
     else:
+        # # Announcement of defeat and the actual word.
+        print(f"lose :(\nThe word was {word}")
 
-        if 8 >= initiate >= 5:
-            stat, i, word = game(initiate)
-            if stat:
-                print(f"You won!\nNumber of attempt: {i + 1}")
-            else:
-                print(f"lose :(\nThe word was {word}")
+# the main program.
+def main():
+
+    # making sure that there's only one commandline argument.
+    if len(sys.argv) == 2:
+
+        try:
+            # making sure that it is an integer.
+            initiate = int(sys.argv[1])
+        except:
+            # showing error and ending the program because the entry is not an integer.
+            print_with_background_color("Invalid entry! try an integer between 5 - 8.", 'red', new_line=True)
+            print_with_background_color("Usage: ./wordle.py [wordsize]", 'blue', new_line=True)
         else:
-            print_with_background_color("Invalid entry!", 'red', new_line=True)
-            print_with_background_color("Error: wordsize must be either 5, 6, 7, or 8", 'blue', new_line=True)
+
+            if 8 >= initiate >= 5:
+                # initiating the game with the chosen mode and showing the results.
+                result, attepts , word = game(initiate)
+                #showing the results
+                show_results(result, attepts , word)
+            else:
+                # showing error and ending the program because the entry is not in the desired range.
+                print_with_background_color("Invalid entry!", 'red', new_line=True)
+                print_with_background_color("Error: wordsize must be either 5, 6, 7, or 8", 'blue', new_line=True)
+    else:
+            # showing error and ending the program because the number of commandline arguments is not valid.
+            print_with_background_color("Invalid number of arguments!", 'red', new_line=True)
+            print_with_background_color("Usage: ./wordle.py [wordsize]", 'blue', new_line=True)
 
 
 
